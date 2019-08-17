b6029cd0db8b916fb9e8df55d4f8956f589ec4dd
hutnyk <jacob.hutnyk@gmail.com>
2019-08-17 17:47:45 -0400

    initial commit: eval and reproduce are working

---
+++ graph.py
@@ -0,0 +1,91 @@
+import random
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+
+# evaluate strong regularity (99,14,1,2)
+def eval(graph):
+	g = nx.adjacency_matrix(graph).todense()
+	fitness = 0
+	for i in range(len(g)):
+		for j in range(i + 1, 99, 1):
+			count = 0
+			for c in range(99):
+				if c == i or c == j:
+					continue
+				if g[i,c] and g[j,c]:
+					count += 1
+			fitness -= abs(count - (1 if g[i,j] else 2))
+	return fitness
+
+def reproduce(first, second):
+	a = nx.to_numpy_matrix(first)
+	b = nx.to_numpy_matrix(second)
+	r = np.zeros(shape=(99,99))
+
+	l = list(range(99))
+	random.shuffle(l)
+
+	full = [0]*99
+	for i in l:
+		poss = []
+		for j in range(99):
+			if a[i,j] or b[i,j]:
+				poss.append(j)
+		random.shuffle(poss)
+		j = 0
+		for j in poss:
+			if full[i] >= 14:
+				break
+			if (not r[i,j]) and full[j] < 14:
+				r[i,j] = 1
+				r[j,i] = 1
+				full[i] += 1
+				full[j] += 1
+		if full[i] < 14:
+			poss = list(set(range(99)).difference(set(poss)))
+			for j in poss:
+				if full[i] >= 14:
+					break
+				if (not r[i,j]) and full[j] < 14:
+					r[i,j] = 1
+					r[j,i] = 1
+					full[i] += 1
+					full[j] += 1
+		if full[i] < 14:
+			raise Exception('Failed to reproduce') 
+	return nx.from_numpy_matrix(r)
+
+# verify 14-regularity
+def verify(g):
+	r = nx.degree_histogram(g)
+	if len(r) == 15 and r[14] == 99:
+		return True
+	return False
+
+def test():
+	better = 0
+	worse = 0
+	fail = 0
+	for x in range(500):
+		a = nx.random_regular_graph(14, 99)
+		b = nx.random_regular_graph(14, 99)
+		av = eval(a)
+		bv = eval(b)
+
+		try:
+			r = reproduce(a,b)
+			rv = eval(b)
+			if rv > av and rv > bv:
+				better += 1
+			elif rv < av and rv < bv:
+				worse += 1
+		except:
+			fail += 1
+	
+	print('better ' + str(better))
+	print('worse ' + str(worse))
+	print('fail ' + str(fail))
+
+if __name__ == "__main__":
+	test()
